fe7e13038f03bc38933f0a40f444723bb2841775
NievesDominguez <mnieves.domnav@educa.jcyl.es>
2026-09-22 11:05:22 +0200

    Cambiados botones

--- yakuzadle/src/App.jsx
+++ yakuzadle/src/App.jsx
@@ -520,7 +520,7 @@ function App() {
             </div>
           ) : (    
             <div className="waiting-message">    
-              🎉 You got it! 🎉    
+              <p>🎉 You got it! 🎉</p>
               <button className="guess-button" onClick={handleShare}>    
                 📋 Share result    
               </button>    
